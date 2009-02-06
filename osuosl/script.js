afd1e5acb55adc1181b2715e958c825bcc0a62f6
Greg Lund-Chaix <gchaix@osuosl.org>
2009-02-06 00:58:45 +0000

    jquery donate button hover effect
    git-svn-id: https://trac.osuosl.org/svn/osl-d6-theme@66 c9f44b58-19b6-4b3e-80f6-ddd52f32771b

--- osuosl/script.js
+++ osuosl/script.js
@@ -7,4 +7,15 @@ Drupal.behaviors.osuosl = function (context) {
     var randSpace = Math.floor(Math.random() * spaceReplace.length);
 //    $newElement = '<h1 id="site-name"><a rel="home" title="Home" href="/">' + openBracket[randBracket] + 'open' + spaceReplace[randSpace] + 'source' + spaceReplace[randSpace] + 'lab' + closeBracket[randBracket] +'</a></h1>';
 //    $("#site-name").before($newElement).remove(); 
+$("#gift_btn input").hover(
+ function()
+  {
+    this.src = this.src.replace("gift_btn.png","gift_btn-hover.png");
+  },
+ function()
+  {
+    this.src = this.src.replace("gift_btn-hover.png","gift_btn.png");
+    alert('hover');
+  }
+);
 };
