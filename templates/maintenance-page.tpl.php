0b48072eb1a5d7d08ca19d6ad99a2670b3283901
Mitchill <mitchell@osuosl.org>
2013-03-15 15:46:29 -0700

    deleted Bartik theme notes from template files.

--- templates/maintenance-page.tpl.php
+++ templates/maintenance-page.tpl.php
@@ -1,16 +1,3 @@
-<?php
-
-/**
- * @file
- * Implementation to display a single Drupal page while offline.
- *
- * All the available variables are mirrored in page.tpl.php.
- *
- * @see template_preprocess()
- * @see template_preprocess_maintenance_page()
- * @see bartik_process_maintenance_page()
- */
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
