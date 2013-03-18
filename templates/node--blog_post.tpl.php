1c509bcf9c4cf72a7798ad80933db5c06f4b9b7c
Mitchill <mitchell@osuosl.org>
2013-03-18 16:09:37 -0700

    print article image under byline, instead of under article

--- templates/node--blog_post.tpl.php
+++ templates/node--blog_post.tpl.php
@@ -11,15 +11,17 @@
     	</div>
     </div>
   <?php endif; ?>
-
-  <div class="content clearfix"<?php print $content_attributes; ?>>
+  
+  
+   <?php print render($content['field_article_image']); ?>
+  
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+      hide($content['field_article_image']);
       print render($content);
     ?>
     
-  </div>
 
 </div>
