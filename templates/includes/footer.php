5357e08fa563b062d4965db928a5d35c15736dc9
Mitchill <mitchell@osuosl.org>
2013-03-20 16:37:46 -0700

    footer update

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -5,15 +5,21 @@
 		</div>
 		
 		<div id="column2">
+			<h4>Site Map</h4>
+			<hr class="decorative" />
 			<?php print render($page['footer_column2']); ?>
 		</div>
 		
 		<div id="column3">
+			<h4>&nbsp;</h4>
+			<hr class="decorative" />
 			<?php print render($page['footer_column3']); ?>
 		</div>
 		
 		<div id="column4">
 			<a href="http://osuosl.org"><img src="/sites/default/themes/osuosl-drupal7/images/logo-full.png" alt="Open Source Lab Logo" /></a>
+			<hr class="decorative" />
+			<p class="footer-type">The Open Source Lab is a nonprofit organization working for the advancement of open source technologies.</p>
 		</div>
 		
 		<div class="clrfix"></div>
