543abdb301281d40b01ab60bc6753c96785ba908
Mitchill <mitchell@osuosl.org>
2013-03-20 16:46:28 -0700

    more consistent footer navigation

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -4,21 +4,20 @@
 			<?php print render($page['footer_column1']); ?>
 		</div>
 		
-		<div id="column2">
-			<h4>Site Map</h4>
-			<hr class="decorative" />
-			<?php print render($page['footer_column2']); ?>
-		</div>
+		<div id="navcontainer">
+				<h4>Site Map</h4>
+				<hr class="decorative" />
+			<div id="column2">
+				<?php print render($page['footer_column2']); ?>
+			</div>
 		
-		<div id="column3">
-			<h4>&nbsp;</h4>
-			<hr class="decorative" />
-			<?php print render($page['footer_column3']); ?>
+			<div id="column3">
+				<?php print render($page['footer_column3']); ?>
+			</div>
 		</div>
 		
 		<div id="column4">
 			<a href="http://osuosl.org"><img src="/sites/default/themes/osuosl-drupal7/images/logo-full.png" alt="Open Source Lab Logo" /></a>
-			<hr class="decorative" />
 			<p class="footer-type">The Open Source Lab is a nonprofit organization working for the advancement of open source technologies.</p>
 		</div>
 		
