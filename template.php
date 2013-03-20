6859f1a789fa14624a3055ba3db8c951cff55a46
Mitchill <mitchell@osuosl.org>
2013-03-20 14:27:54 -0700

    created pre-process to delete image height/width output drupal creates

--- template.php
+++ template.php
@@ -29,4 +29,14 @@ function _phptemplate_variables($hook, $vars = array())
             break;
         }
     }
+    
+function osuosl_preprocess_image(&$variables) {
+  $attributes = &$variables['attributes'];
+
+  foreach (array('width', 'height') as $key) {
+    unset($attributes[$key]);
+    unset($variables[$key]);
+  }
+}
+
 ?>
