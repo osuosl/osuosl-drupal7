9c444da883f9f8abc3453c16e383c5527e2b46f6
Mitchell Garcia <mitchillgarcia@gmail.com>
2013-08-20 16:33:08 -0700

    Deleting line divider between sections

--- templates/node--staff.tpl.php
+++ templates/node--staff.tpl.php
@@ -5,10 +5,11 @@
 		</div>
 		
 	<h2><?php print $title; ?></h2>
-	<h4 class="subtitle"><?php print render($content['field_job_title']); ?> | <?php print render($content['field_joke_job_title']); ?></h4>
+	<h4 class="subtitle"><?php print render($content['field_job_title']); ?> <?php print render($content['field_joke_job_title']); ?></h4>
     <?php
     	hide($content['field_picture']);
       	hide($content['field_job_title']); 
+      	hide($content['field_joke_job_title']); 
       	
       	print render($content); ?>
 </div>
