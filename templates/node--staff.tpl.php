0c18f62039cb70f36c65c8bcc94d3e9a775f8368
Mitchell Garcia <mitchillgarcia@gmail.com>
2013-08-20 16:21:34 -0700

    Added joke job titles to staff bios

--- templates/node--staff.tpl.php
+++ templates/node--staff.tpl.php
@@ -5,7 +5,7 @@
 		</div>
 		
 	<h2><?php print $title; ?></h2>
-	<h4 class="subtitle"><?php print render($content['field_job_title']); ?></h4>
+	<h4 class="subtitle"><?php print render($content['field_job_title']); ?> | <?php print render($content['field_joke_job_title']); ?></h4>
     <?php
     	hide($content['field_picture']);
       	hide($content['field_job_title']); 
